72ea8c6c3a513a5453877c25f6dd1bf6e1818bda
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2018-02-13 19:09:04 +0000

    Support yaml serialization of TransactionBlockEvent

--- api/src/main/java/cash/xcl/api/dto/TransactionBlockEvent.java
+++ api/src/main/java/cash/xcl/api/dto/TransactionBlockEvent.java
@@ -1,16 +1,23 @@
 package cash.xcl.api.dto;
 
 import cash.xcl.api.AllMessages;
+import cash.xcl.api.tcp.WritingAllMessages;
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.bytes.BytesIn;
+import net.openhft.chronicle.bytes.BytesOut;
 import net.openhft.chronicle.bytes.BytesStore;
+import net.openhft.chronicle.core.io.IORuntimeException;
+import net.openhft.chronicle.wire.WireIn;
+import net.openhft.chronicle.wire.WireOut;
+import org.jetbrains.annotations.NotNull;
 
 public class TransactionBlockEvent extends SignedMessage {
     private int weekNumber;
     private long blockNumber; // unsigned int
 
-    private Bytes transactions = Bytes.allocateElasticDirect();
+    private transient Bytes transactions = Bytes.allocateElasticDirect();
 
+    private transient int count;
     private transient DtoParser dtoParser;
 
     public TransactionBlockEvent(long sourceAddress, long eventTime, int weekNumber, long blockNumber) {
@@ -25,10 +32,15 @@ public class TransactionBlockEvent extends SignedMessage {
 
     public void clear() {
         transactions.clear();
+        count = 0;
+    }
+
+    public void incrementBlockNumber() {
         blockNumber++;
     }
 
     public void addTransaction(SignedMessage message) {
+        count++;
         message.writeMarshallable(transactions);
     }
 
@@ -40,7 +52,7 @@ public class TransactionBlockEvent extends SignedMessage {
     }
 
     @Override
-    protected void readMarshallable2(BytesIn bytes) {
+    protected void readMarshallable2(BytesIn<?> bytes) {
         weekNumber = bytes.readUnsignedShort();
         blockNumber = bytes.readUnsignedInt();
         if (transactions == null) transactions = Bytes.allocateElasticDirect();
@@ -48,10 +60,41 @@ public class TransactionBlockEvent extends SignedMessage {
     }
 
     @Override
-    protected void writeMarshallable2(Bytes bytes) {
+    protected void writeMarshallable2(BytesOut<?> bytes) {
         bytes.writeUnsignedShort(weekNumber);
         bytes.writeUnsignedInt(blockNumber);
         bytes.write(transactions);
+    }
+
+    @Override
+    public void readMarshallable(@NotNull WireIn wire) throws IORuntimeException {
+        super.readMarshallable(wire);
+        clear();
+        wire.read("transactions").sequence(this, (tbe, in) -> {
+            while (in.hasNextSequenceItem())
+                tbe.addTransaction(in.object(SignedMessage.class));
+        });
+    }
+
+    @Override
+    public void writeMarshallable(@NotNull WireOut wire) {
+        super.writeMarshallable(wire);
+        wire.write("transactions").sequence(out -> replay(new WritingAllMessages() {
+            @Override
+            public AllMessages to(long addressOrRegion) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            protected void write(SignedMessage message) {
+                out.object(message);
+            }
+
+            @Override
+            public void close() {
+                throw new UnsupportedOperationException();
+            }
+        }));
     }
 
     @Override
@@ -80,5 +123,9 @@ public class TransactionBlockEvent extends SignedMessage {
     public TransactionBlockEvent blockNumber(long blockNumber) {
         this.blockNumber = blockNumber;
         return this;
+    }
+
+    public int count() {
+        return count;
     }
 }
---
+++ api/src/test/java/cash/xcl/api/dto/TransactionBlockEventTest.java
@@ -0,0 +1,65 @@
+package cash.xcl.api.dto;
+
+import cash.xcl.api.AllMessages;
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.core.Mocker;
+import net.openhft.chronicle.wire.Marshallable;
+import org.junit.Test;
+
+import java.io.StringWriter;
+
+import static org.junit.Assert.assertEquals;
+
+public class TransactionBlockEventTest {
+
+    @Test
+    public void writeMarshallable() {
+        TransactionBlockEvent tbe = new TransactionBlockEvent();
+        tbe.addTransaction(new CreateNewAddressCommand(1, 2, Bytes.wrapForRead(new byte[32]), "gb1dn"));
+        tbe.addTransaction(new TransferValueCommand(0, 3, 1, 1.23, "XCL", "init"));
+
+        assertEquals("!cash.xcl.api.dto.TransactionBlockEvent {\n" +
+                "  sourceAddress: 0,\n" +
+                "  eventTime: 0,\n" +
+                "  weekNumber: 0,\n" +
+                "  blockNumber: 0,\n" +
+                "  transactions: [\n" +
+                "    !cash.xcl.api.dto.CreateNewAddressCommand { sourceAddress: 1, eventTime: 2, publicKey: !!binary AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=, region: gb1dn },\n" +
+                "    !cash.xcl.api.dto.TransferValueCommand { sourceAddress: 0, eventTime: 3, toAddress: 1, amount: 1.23, currency: XCL, reference: init }\n" +
+                "  ]\n" +
+                "}\n", tbe.toString());
+    }
+
+    @Test
+    public void readMarshallable() {
+        TransactionBlockEvent tbe = Marshallable.fromString("!cash.xcl.api.dto.TransactionBlockEvent {\n" +
+                "  sourceAddress: 0,\n" +
+                "  eventTime: 0,\n" +
+                "  weekNumber: 0,\n" +
+                "  blockNumber: 0,\n" +
+                "  transactions: [\n" +
+                "    !cash.xcl.api.dto.CreateNewAddressCommand { sourceAddress: 1, eventTime: 2, publicKey: !!binary AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=, region: gb1dn },\n" +
+                "    !cash.xcl.api.dto.TransferValueCommand { sourceAddress: 0, eventTime: 3, toAddress: 1, amount: 1.23, currency: XCL, reference: init }\n" +
+                "  ]\n" +
+                "}\n");
+        StringWriter out = new StringWriter();
+        tbe.replay(Mocker.logging(AllMessages.class, "out ", out));
+        assertEquals("out createNewAddressCommand[!cash.xcl.api.dto.CreateNewAddressCommand {\n" +
+                "  sourceAddress: 1,\n" +
+                "  eventTime: 2,\n" +
+                "  publicKey: !!binary AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=,\n" +
+                "  region: gb1dn\n" +
+                "}\n" +
+                "]\n" +
+                "out transferValueCommand[!cash.xcl.api.dto.TransferValueCommand {\n" +
+                "  sourceAddress: 0,\n" +
+                "  eventTime: 3,\n" +
+                "  toAddress: 1,\n" +
+                "  amount: 1.23,\n" +
+                "  currency: XCL,\n" +
+                "  reference: init\n" +
+                "}\n" +
+                "]\n", out.toString());
+
+    }
+}
