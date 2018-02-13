f3091a1ae304b1486ea914836e29317c61540045
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2018-02-13 13:47:59 +0000

    Add create new address and transfer to RegionalServer

--- server/src/main/java/cash/xcl/server/RegionalServer.java
+++ server/src/main/java/cash/xcl/server/RegionalServer.java
@@ -1,4 +1,246 @@
 package cash.xcl.server;
 
-public class RegionalServer {
+import cash.xcl.api.AllMessages;
+import cash.xcl.api.AllMessagesLookup;
+import cash.xcl.api.ServerComponent;
+import cash.xcl.api.dto.*;
+import cash.xcl.api.util.XCLBase32;
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.core.time.SystemTimeProvider;
+import net.openhft.chronicle.core.time.TimeProvider;
+
+public class RegionalServer implements ServerComponent {
+    private final AddressService addressService = new AddressService();
+    private long serverAddress;
+    private TimeProvider timeProvider = SystemTimeProvider.INSTANCE;
+    private AllMessagesLookup lookup;
+
+    @Override
+    public void allMessagesLookup(AllMessagesLookup lookup) {
+        this.lookup = lookup;
+    }
+
+    @Override
+    public void createNewAddressCommand(CreateNewAddressCommand createNewAddressCommand) {
+        String region = XCLBase32.normalize(createNewAddressCommand.region());
+        try {
+            Bytes publicKey = createNewAddressCommand.publicKey();
+            long address = addressService.createAddress(region, publicKey);
+            lookup.to(createNewAddressCommand.sourceAddress())
+                    .createNewAddressEvent(
+                            new CreateNewAddressEvent(serverAddress(), eventTime(), createNewAddressCommand, address, publicKey));
+
+        } catch (IllegalArgumentException iae) {
+            lookup.to(createNewAddressCommand.sourceAddress())
+                    .commandFailedEvent(new CommandFailedEvent(serverAddress(), eventTime(), createNewAddressCommand, iae.getMessage()));
+        }
+    }
+
+    @Override
+    public void transferValueCommand(TransferValueCommand transferValueCommand) {
+        TransferValueEvent event = new TransferValueEvent(serverAddress, eventTime(), transferValueCommand);
+        lookup.to(transferValueCommand.sourceAddress())
+                .transferValueEvent(event);
+        lookup.to(transferValueCommand.toAddress())
+                .transferValueEvent(event);
+    }
+
+    private long eventTime() {
+        return timeProvider.currentTimeMicros();
+    }
+
+
+    public long serverAddress() {
+        return serverAddress;
+    }
+
+    @Override
+    public AllMessages to(long addressOrRegion) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void commandFailedEvent(CommandFailedEvent commandFailedEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void queryFailedResponse(QueryFailedResponse queryFailedResponse) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void createNewAddressEvent(CreateNewAddressEvent createNewAddressEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void transferValueEvent(TransferValueEvent transferValueEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clusterTransferStep3Event(ClusterTransferStep3Event clusterTransferStep3Event) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void depositValueEvent(DepositValueEvent depositValueEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void withdrawValueEvent(WithdrawValueEvent withdrawValueEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void executionReportEvent(ExecutionReportEvent executionReportEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void subscriptionSuccessResponse(SubscriptionSuccessResponse subscriptionSuccessResponse) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clusterStatusResponse(ClusterStatusResponse clusterStatusResponse) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clustersStatusResponse(ClustersStatusResponse clustersStatusResponse) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void currentBalanceResponse(CurrentBalanceResponse currentBalanceResponse) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void exchangeRateResponse(ExchangeRateResponse exchangeRateResponse) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clusterTransferStep1Command(ClusterTransferStep1Command clusterTransferStep1Command) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void subscriptionQuery(SubscriptionQuery subscriptionQuery) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void newLimitOrderCommand(NewLimitOrderCommand newLimitOrderCommand) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void newMarketOrderCommand(NewMarketOrderCommand newMarketOrderCommand) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void cancelOrderCommand(CancelOrderCommand cancelOrderCommand) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clusterStatusQuery(ClusterStatusQuery clusterStatusQuery) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clustersStatusQuery(ClustersStatusQuery clustersStatusQuery) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void currentBalanceQuery(CurrentBalanceQuery currentBalanceQuery) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void exchangeRateQuery(ExchangeRateQuery exchangeRateQuery) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void blockSubscriptionQuery(BlockSubscriptionQuery blockSubscriptionQuery) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clusterTransferStep2Command(ClusterTransferStep2Command clusterTransferStep2Command) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clusterTransferStep3Command(ClusterTransferStep3Command clusterTransferStep3Command) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void depositValueCommand(DepositValueCommand depositValueCommand) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void withdrawValueCommand(WithdrawValueCommand withdrawValueCommand) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void applicationMessageEvent(ApplicationMessageEvent applicationMessageEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void transactionBlockEvent(TransactionBlockEvent transactionBlockEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void transactionBlockGossipEvent(TransactionBlockGossipEvent transactionBlockGossipEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void treeBlockEvent(TreeBlockEvent treeBlockEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void feesEvent(FeesEvent feesEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void exchangeRateEvent(ExchangeRateEvent exchangeRateEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void openingBalanceEvent(OpeningBalanceEvent openingBalanceEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void currentBalanceEvent(CurrentBalanceResponse currentBalanceResponse) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void serviceNodesEvent(ServiceNodesEvent serviceNodesEvent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void close() {
+        throw new UnsupportedOperationException();
+    }
 }
